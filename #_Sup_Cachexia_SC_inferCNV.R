d98778ba543957806c9d855c924141d56b63ed3f
CharleneMSI <q2330416@gmail.com>
2022-08-15 14:50:40 +0800

    Comment out the example.

--- #_Sup_Cachexia_SC_inferCNV.R
+++ #_Sup_Cachexia_SC_inferCNV.R
@@ -1,5 +1,4 @@
 ##### version info #####
-
   # platform       x86_64-w64-mingw32
   # arch           x86_64
   # os             mingw32
@@ -21,20 +20,20 @@ library(infercnv)
   RVersion = "20220114_inferCNV"
   dir.create(paste0(PathName,"/",RVersion))
 
-##### Example #####
-
-  infercnv_obj = CreateInfercnvObject(raw_counts_matrix=system.file("extdata", "oligodendroglioma_expression_downsampled.counts.matrix.gz", package = "infercnv"),
-                                      annotations_file=system.file("extdata", "oligodendroglioma_annotations_downsampled.txt", package = "infercnv"),
-                                      delim="\t",
-                                      gene_order_file=system.file("extdata", "gencode_downsampled.EXAMPLE_ONLY_DONT_REUSE.txt", package = "infercnv"),
-                                      ref_group_names=c("Microglia/Macrophage","Oligodendrocytes (non-malignant)"))
-
-  infercnv_obj = infercnv::run(infercnv_obj,
-                               cutoff=1, # cutoff=1 works well for Smart-seq2, and cutoff=0.1 works well for 10x Genomics
-                               out_dir=tempfile(),
-                               cluster_by_groups=TRUE,
-                               denoise=TRUE,
-                               HMM=TRUE)
+# ##### Example #####
+#
+#   infercnv_obj = CreateInfercnvObject(raw_counts_matrix=system.file("extdata", "oligodendroglioma_expression_downsampled.counts.matrix.gz", package = "infercnv"),
+#                                       annotations_file=system.file("extdata", "oligodendroglioma_annotations_downsampled.txt", package = "infercnv"),
+#                                       delim="\t",
+#                                       gene_order_file=system.file("extdata", "gencode_downsampled.EXAMPLE_ONLY_DONT_REUSE.txt", package = "infercnv"),
+#                                       ref_group_names=c("Microglia/Macrophage","Oligodendrocytes (non-malignant)"))
+#
+#   infercnv_obj = infercnv::run(infercnv_obj,
+#                                cutoff=1, # cutoff=1 works well for Smart-seq2, and cutoff=0.1 works well for 10x Genomics
+#                                out_dir=tempfile(),
+#                                cluster_by_groups=TRUE,
+#                                denoise=TRUE,
+#                                HMM=TRUE)
 
 #######################
   ## Exp
