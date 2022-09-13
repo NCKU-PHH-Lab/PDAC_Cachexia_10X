266e6b5945827b7540679f8b703a6fcdd750b1e3
CharleneMSI <q2330416@gmail.com>
2022-09-13 12:54:12 +0800

    Change BBPlot name

--- FUN_GSEA_ExtractSubType.R
+++ FUN_GSEA_ExtractSubType.R
@@ -13,28 +13,29 @@ GSEA_ExtractSubType = function(GSEA_Large.Sum.TOP.S,
   }
 
   ## Bubble plot
-  BBPlot_T <- ggplot(GSEA_Sub.df,aes(x=PhenoType, y = pathway, color = NES, size = -log10(padj))) +
+  BBPlot_Sub <- ggplot(GSEA_Sub.df,aes(x=PhenoType, y = pathway, color = NES, size = -log10(padj))) +
               geom_point() +scale_size_area(max_size = 7)+
               scale_colour_gradient2(low = GSEA_Color.lt[["low"]], mid = GSEA_Color.lt[["mid"]], high = GSEA_Color.lt[["high"]],
                                      guide = "colourbar",midpoint = 0)+ theme(legend.position = "bottom")+ theme_bw()+
               theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank())
 
-  BBPlot_TB <- BBPlot_T %>% BeautifyggPlot(LegPos  = "bottom",LegBox = "horizontal",LegDir="horizontal", xangle =90,OL_Thick = 1.5,
+  BBPlot_SubB <- BBPlot_Sub %>% BeautifyggPlot(LegPos  = "bottom",LegBox = "horizontal",LegDir="horizontal", xangle =90,OL_Thick = 1.5,
                                            XtextSize=15 ,  YtextSize=10,AxisTitleSize=1, AspRat=4, XaThick=0.8, YaThick=0.8)
-  # BBPlot_TB <- BBPlot_TB +theme(axis.title.y=element_blank(),
+  # BBPlot_SubB <- BBPlot_SubB +theme(axis.title.y=element_blank(),
   #                  axis.text.y=element_blank(),
   #                  axis.ticks.y=element_blank())
 
-  BBPlot_TB
+  BBPlot_SubB
 
-  BBPlot_TB1 <- BBPlot_TB %>%
+  ## Sort the plot
+  BBPlot_SubB_Sort <- BBPlot_SubB %>%
                 insert_left(GSEA_ggplot_SPA.lt[["Y_Order"]],width = 0.2)
-  BBPlot_TB1
+  BBPlot_SubB_Sort
 
 
   pdf(file = paste0(Subfolder.Path,"/PBMC_GSEA/PBMC_GSEA_Bubble_SPA_SubType_T.pdf"),width = 17, height = 7 )
-  BBPlot_TB
-  BBPlot_TB1
+    BBPlot_SubB
+    BBPlot_SubB_Sort
   dev.off()
 
   return(GSEA_BBPlot.lt)
