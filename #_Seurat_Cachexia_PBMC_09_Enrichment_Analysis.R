a4aa01ea4dcc595298a7c6c18cb966916bfbe8de
CharleneMSI <q2330416@gmail.com>
2022-09-13 14:01:15 +0800

    GSEA_T.lt setting

--- #_Seurat_Cachexia_PBMC_09_Enrichment_Analysis.R
+++ #_Seurat_Cachexia_PBMC_09_Enrichment_Analysis.R
@@ -146,42 +146,14 @@ dev.off()
 source("FUN_GSEA_ExtractSubType.R")
 
 ## T Cell
-GSEA_ExtractSubType(GSEA_Large.Sum.TOP.S,
-                    KeyWordSet.lt = list(Mode = "KWSet", KW = c("CD4+T","CD8+T","T")),
-                    Order.lt = GSEA_ggplot_SPA.lt,
-                    GSEA_Color = GSEA_Color.lt,
-                    Save.Path = Subfolder.Path)
+GSEA_T.lt <- GSEA_ExtractSubType(GSEA_Large.Sum.TOP.S,
+                                 KeyWordSet.lt = list(Mode = "KWSet", KW = c("CD4+T","CD8+T","T")),
+                                 Order.lt = GSEA_ggplot_SPA.lt,
+                                 GSEA_Color = GSEA_Color.lt,
+                                 Save.Path = paste0(Subfolder.Path))
+BBPlot_TB <- GSEA_T.lt[["BBPlot_SubB"]]
+BBPlot_TB1 <- GSEA_T.lt[["BBPlot_SubB_Sort"]]
 
-
-
-## T Cell
-# GSEA_T.df <- GSEA_Large.Sum.TOP.S[grep("T",GSEA_Large.Sum.TOP.S$PhenoType),]
-GSEA_T.df <- GSEA_Large.Sum.TOP.S[GSEA_Large.Sum.TOP.S$PhenoType %in% c("CD4+T","CD8+T","T"),]
-
-BBPlot_T <- ggplot(GSEA_T.df,aes(x=PhenoType, y = pathway, color = NES, size = -log10(padj))) +
-            geom_point() +scale_size_area(max_size = 7)+
-            scale_colour_gradient2(low = GSEA_Color.lt[["low"]], mid = GSEA_Color.lt[["mid"]], high = GSEA_Color.lt[["high"]],
-                                   guide = "colourbar",midpoint = 0)+ theme(legend.position = "bottom")+ theme_bw()+
-            theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank())
-
-BBPlot_T
-
-BBPlot_TB <- BBPlot_T %>% BeautifyggPlot(LegPos  = "bottom",LegBox = "horizontal",LegDir="horizontal", xangle =90,OL_Thick = 1.5,
-                                         XtextSize=15 ,  YtextSize=10,AxisTitleSize=1, AspRat=4, XaThick=0.8, YaThick=0.8)
-# BBPlot_TB <- BBPlot_TB +theme(axis.title.y=element_blank(),
-#                  axis.text.y=element_blank(),
-#                  axis.ticks.y=element_blank())
-BBPlot_TB
-
-BBPlot_TB1 <- BBPlot_TB %>%
-  insert_left(GSEA_ggplot_SPA.lt[["Y_Order"]],width = 0.2)
-BBPlot_TB1
-
-
-pdf(file = paste0(Subfolder.Path,"/PBMC_GSEA/PBMC_GSEA_Bubble_SPA_SubType_T.pdf"),width = 17, height = 7 )
-  BBPlot_TB
-  BBPlot_TB1
-dev.off()
 
 
 ## Mac
