4d71a992013f4c4d12c9dddb9513d74f89462fa2
CharleneMSI <q2330416@gmail.com>
2022-09-13 19:36:42 +0800

    Clean up the code

--- #_Seurat_Cachexia_PBMC_09_Enrichment_Analysis.R
+++ #_Seurat_Cachexia_PBMC_09_Enrichment_Analysis.R
@@ -52,78 +52,13 @@ if (!dir.exists(Subfolder.Path)){
 dir.create(paste0(Subfolder.Path))
 
 ## GSEA analysis
-
 GSEA_SPA.lt <- GSEA_Run_Multi(CCMarker_SPA.lt,
                               GeneSets = Pathway.all.MM, TopNum = 10,
                               Save.Path = Subfolder.Path, FileName = "/PBMC_GSEA_SPA_EnrichPlot.pdf")
 
 GSEA_Large.df.TOP <- GSEA_SPA.lt[["GSEA_Large.df.TOP"]]
-
-
-# GSEA_Large <- list()
-# GSEA_Large.df <- as.data.frame(matrix(nrow=0,ncol=10))
-# colnames(GSEA_Large.df) <- c("GeneType","PhenoType","pathway","pval","padj","log2err","ES", "NES" ,"size","leadingEdge")
-# GSEA_Large.df.TOP <- GSEA_Large.df
-#
-# pdf(file = paste0(Subfolder.Path, "/PBMC_GSEA_SPA.pdf"),width = 15, height = 7 )
-#
-# for(i in 1:length(CellType.list)){
-#
-#   gseaDat <- CCMarker_SPA.lt[[paste0(CellType.list[i])]][["CCMarker.All"]]
-#   gseaDat <- data.frame(row.names(gseaDat),gseaDat)
-#   colnames(gseaDat)[[1]] <- c("Gene")
-#   ranks <- gseaDat$avg_log2FC
-#   names(ranks) <- gseaDat$Gene
-#   # head(ranks)
-#   # barplot(sort(ranks, decreasing = T))
-#
-#   GSEA_Large.Output <- GSEA_Run_LargeGeneSet(ranks,Pathway.all.MM,10)
-#
-#   fgseaRes <- GSEA_Large.Output[["fgseaRes"]]
-#   # head(fgseaRes[order(padj, -abs(NES)), ], n=10)
-#
-#   pathwaysH <- GSEA_Large.Output[["Pathway.all.list"]]
-#
-#   # plot.new()
-#   # plotEnrichment(pathwaysH[[as.character(fgseaRes$pathway[1])]], ranks)
-#
-#   topPathways <- GSEA_Large.Output[["topPathways"]]
-#
-#   library(ggplot2)
-#   plot.new()
-#   plotGseaTable(pathwaysH[topPathways$pathway],
-#                 ranks,
-#                 fgseaRes,
-#                 gseaParam = 0.5) + title( paste0("PBMC.",CellType.list[i]), adj = 0, line =3)
-#
-#   plotEnrichment_Pos1 <- plotEnrichment(pathwaysH[[as.character(topPathways[1,1])]], ranks)+ labs(title= paste0("PBMC.",CellType.list[i],": ",as.character(topPathways[1,1])))
-#   #plotEnrichment_Pos1
-#   plotEnrichment_Neg1 <- plotEnrichment(pathwaysH[[as.character(topPathways[length(as.data.frame(topPathways)[,1]),1])]], ranks)+ labs(title= paste0("PBMC.",CellType.list[i],": ",as.character(topPathways[length(as.data.frame(topPathways)[,1]),1])))
-#   #plotEnrichment_Neg1
-#
-#   Sum <- list(gseaDat,ranks,pathwaysH,fgseaRes,plotEnrichment_Pos1,plotEnrichment_Neg1)
-#   names(Sum) <- c("gseaDat","ranks","pathwaysH","fgseaRes","plotEnrichment_Pos1","plotEnrichment_Neg1")
-#   GSEA_Large[[i]] <- Sum
-#   names(GSEA_Large)[[i]] <- paste0(CellType.list[i])
-#
-#   fgseaRes2 <- data.frame(paste0(CellType.list[i]),fgseaRes)
-#   colnames(fgseaRes2)[[1]] <- c("PhenoType")
-#   GSEA_Large.df <- rbind(GSEA_Large.df,fgseaRes2 )
-#
-#   topPathways2 <- data.frame(paste0(CellType.list[i]),topPathways)
-#   colnames(topPathways2)[[1]] <- c("PhenoType")
-#   GSEA_Large.df.TOP <- rbind(GSEA_Large.df.TOP, topPathways2)
-#
-#   rm(gseaDat,ranks,pathwaysH,fgseaRes,fgseaRes2,plotEnrichmen,Sum,topPathways,topPathways2)
-#
-# }
-#
-# dev.off()
-
-## GSEA_Large.Sum.TOP ##
-GSEA_Large.Sum.TOP <- rbind(GSEA_Large.df.TOP)
-GSEA_Large.Sum.TOP <- GSEA_Large.Sum.TOP[,!colnames(GSEA_Large.Sum.TOP) %in% c("leadingEdge")]
-write.table(GSEA_Large.Sum.TOP, file=paste0(Subfolder.Path,"/PBMC_GSEA_Pathway_LargeTOP_SPA.txt"),sep="\t",
+GSEA_Large.df.TOP <- GSEA_Large.df.TOP[,!colnames(GSEA_Large.df.TOP) %in% c("leadingEdge")]
+write.table(GSEA_Large.df.TOP, file=paste0(Subfolder.Path,"/PBMC_GSEA_Pathway_LargeTOP_SPA.txt"),sep="\t",
             row.names=F, quote = FALSE)
 
 ##### Bubble plot #####
@@ -178,77 +113,16 @@ rm(p2,p3,BBPlotB1,BBPlotB2,BBPlotB,BBPlot_Cluster,df1.1.clust.Pheno,df1.1.clust.
 save.image(paste0(Subfolder.Path,"/09_1_GSEA_Analysis_(SPA).RData"))
 
 ##*******************************************************************************************************************##
-##### 09_2 GSEA Analysis (SSA_MAle) #####
-GSEA_Large_Male <- list()
-GSEA_Large_Male.df <- as.data.frame(matrix(nrow=0,ncol=10))
-colnames(GSEA_Large_Male.df) <- c("GeneType","PhenoType","pathway","pval","padj","log2err","ES", "NES" ,"size","leadingEdge")
-GSEA_Large_Male.df.TOP <- GSEA_Large_Male.df
+##### 09_2 GSEA Analysis (SSA_Male) #####
+## GSEA analysis
+GSEA_SSA_Male.lt <- GSEA_Run_Multi(CCMarker_Male.lt,
+                              GeneSets = Pathway.all.MM, TopNum = 10,
+                              Save.Path = Subfolder.Path, FileName = "/PBMC_GSEA_SSA_Male_EnrichPlot.pdf")
 
-
-pdf(file = paste0(Subfolder.Path, "/PBMC_GSEA_SSA_Male.pdf"),width = 15, height = 7 )
-
-for(i in 1:length(CellType.list)){
-
-  gseaDat <- CCMarker_Male.lt[[paste0(CellType.list[i])]][["CCMarker.All"]]
-  gseaDat <- data.frame(row.names(gseaDat),gseaDat)
-  colnames(gseaDat)[[1]] <- c("Gene")
-  ranks <- gseaDat$avg_log2FC
-  names(ranks) <- gseaDat$Gene
-  # head(ranks)
-  # barplot(sort(ranks, decreasing = T))
-
-
-  #GSEA_Large_Male.Output <- GSEA_Run_LargeGeneSet(ranks,Pathway.all,10)
-  GSEA_Large_Male.Output <- GSEA_Run_LargeGeneSet(ranks,Pathway.all.MM,10)
-
-  fgseaRes <- GSEA_Large_Male.Output[["fgseaRes"]]
-  # head(fgseaRes[order(padj, -abs(NES)), ], n=10)
-
-  pathwaysH <- GSEA_Large_Male.Output[["Pathway.all.list"]]
-
-  # plot.new()
-  # plotEnrichment(pathwaysH[[as.character(fgseaRes$pathway[1])]], ranks)
-
-  topPathways <- GSEA_Large_Male.Output[["topPathways"]]
-
-  library(ggplot2)
-  plot.new()
-  plotGseaTable(pathwaysH[topPathways$pathway],
-                ranks,
-                fgseaRes,
-                gseaParam = 0.5) + title( paste0("PBMC.",CellType.list[i]), adj = 0, line =3)
-
-  plotEnrichment_Pos1 <- plotEnrichment(pathwaysH[[as.character(topPathways[1,1])]], ranks)+ labs(title= paste0("PBMC.",CellType.list[i],": ",as.character(topPathways[1,1])))
-  #plotEnrichment_Pos1
-  plotEnrichment_Neg1 <- plotEnrichment(pathwaysH[[as.character(topPathways[length(as.data.frame(topPathways)[,1]),1])]], ranks)+ labs(title= paste0("PBMC.",CellType.list[i],": ",as.character(topPathways[length(as.data.frame(topPathways)[,1]),1])))
-  #plotEnrichment_Neg1
-
-  Sum <- list(gseaDat,ranks,pathwaysH,fgseaRes,plotEnrichment_Pos1,plotEnrichment_Neg1)
-  names(Sum) <- c("gseaDat","ranks","pathwaysH","fgseaRes","plotEnrichment_Pos1","plotEnrichment_Neg1")
-  GSEA_Large_Male[[i]] <- Sum
-  names(GSEA_Large_Male)[[i]] <- paste0(CellType.list[i])
-
-  fgseaRes2 <- data.frame(paste0(CellType.list[i]),fgseaRes)
-  colnames(fgseaRes2)[[1]] <- c("PhenoType")
-  GSEA_Large_Male.df <- rbind(GSEA_Large_Male.df,fgseaRes2 )
-
-  topPathways2 <- data.frame(paste0(CellType.list[i]),topPathways)
-  colnames(topPathways2)[[1]] <- c("PhenoType")
-  GSEA_Large_Male.df.TOP <- rbind(GSEA_Large_Male.df.TOP,topPathways2 )
-
-  rm(gseaDat,ranks,pathwaysH,fgseaRes,fgseaRes2,plotEnrichmen,Sum,topPathways,topPathways2)
-
-}
-
-dev.off()
-
-## GSEA_Large_Male.Sum.TOP ##
-GSEA_Large_Male.Sum.TOP <- rbind(GSEA_Large_Male.df.TOP)
-GSEA_Large_Male.Sum.TOP <- GSEA_Large_Male.Sum.TOP[,!colnames(GSEA_Large_Male.Sum.TOP) %in% c("leadingEdge")]
-write.table(GSEA_Large_Male.Sum.TOP, file=paste0(Subfolder.Path,"/PBMC_GSEA_Pathway_LargeTOP_SSA_Male.txt"),sep="\t",
+GSEA_Large_Male.df.TOP <- GSEA_SSA_Male.lt[["GSEA_Large.df.TOP"]]
+GSEA_Large_Male.df.TOP <- GSEA_Large_Male.df.TOP[,!colnames(GSEA_Large_Male.df.TOP) %in% c("leadingEdge")]
+write.table(GSEA_Large_Male.df.TOP, file=paste0(Subfolder.Path,"/PBMC_GSEA_Pathway_LargeTOP_SSA_Male.txt"),sep="\t",
             row.names=F, quote = FALSE)
-
-
 
 ##### Bubble plot #####
 library(ggplot2)
@@ -294,73 +168,14 @@ save.image(paste0(Subfolder.Path,"/09_2_GSEA_Analysis_(SSA_Male).RData"))
 
 ##*******************************************************************************************************************##
 ##### 09_3 GSEA Analysis (SSA_Female) #####
-GSEA_Large_Female <- list()
-GSEA_Large_Female.df <- as.data.frame(matrix(nrow=0,ncol=10))
-colnames(GSEA_Large_Female.df) <- c("GeneType","PhenoType","pathway","pval","padj","log2err","ES", "NES" ,"size","leadingEdge")
-GSEA_Large_Female.df.TOP <- GSEA_Large_Female.df
+## GSEA analysis
+GSEA_SSA_Female.lt <- GSEA_Run_Multi(CCMarker_Female.lt,
+                                   GeneSets = Pathway.all.MM, TopNum = 10,
+                                   Save.Path = Subfolder.Path, FileName = "/PBMC_GSEA_SSA_Female_EnrichPlot.pdf")
 
-
-pdf(file = paste0(Subfolder.Path, "/PBMC_GSEA_SSA_Female.pdf"),width = 15, height = 7 )
-
-for(i in 1:length(CellType.list)){
-
-  gseaDat <- CCMarker_Female.lt[[paste0(CellType.list[i])]][["CCMarker.All"]]
-  gseaDat <- data.frame(row.names(gseaDat),gseaDat)
-  colnames(gseaDat)[[1]] <- c("Gene")
-  ranks <- gseaDat$avg_log2FC
-  names(ranks) <- gseaDat$Gene
-  # head(ranks)
-  # barplot(sort(ranks, decreasing = T))
-
-
-  #GSEA_Large_Female.Output <- GSEA_Run_LargeGeneSet(ranks,Pathway.all,10)
-  GSEA_Large_Female.Output <- GSEA_Run_LargeGeneSet(ranks,Pathway.all.MM,10)
-
-  fgseaRes <- GSEA_Large_Female.Output[["fgseaRes"]]
-  # head(fgseaRes[order(padj, -abs(NES)), ], n=10)
-
-  pathwaysH <- GSEA_Large_Female.Output[["Pathway.all.list"]]
-
-  # plot.new()
-  # plotEnrichment(pathwaysH[[as.character(fgseaRes$pathway[1])]], ranks)
-
-  topPathways <- GSEA_Large_Female.Output[["topPathways"]]
-
-  library(ggplot2)
-  plot.new()
-  plotGseaTable(pathwaysH[topPathways$pathway],
-                ranks,
-                fgseaRes,
-                gseaParam = 0.5) + title( paste0("PBMC.",CellType.list[i]), adj = 0, line =3)
-
-  plotEnrichment_Pos1 <- plotEnrichment(pathwaysH[[as.character(topPathways[1,1])]], ranks)+ labs(title= paste0("PBMC.",CellType.list[i],": ",as.character(topPathways[1,1])))
-  #plotEnrichment_Pos1
-  plotEnrichment_Neg1 <- plotEnrichment(pathwaysH[[as.character(topPathways[length(as.data.frame(topPathways)[,1]),1])]], ranks)+ labs(title= paste0("PBMC.",CellType.list[i],": ",as.character(topPathways[length(as.data.frame(topPathways)[,1]),1])))
-  #plotEnrichment_Neg1
-
-  Sum <- list(gseaDat,ranks,pathwaysH,fgseaRes,plotEnrichment_Pos1,plotEnrichment_Neg1)
-  names(Sum) <- c("gseaDat","ranks","pathwaysH","fgseaRes","plotEnrichment_Pos1","plotEnrichment_Neg1")
-  GSEA_Large_Female[[i]] <- Sum
-  names(GSEA_Large_Female)[[i]] <- paste0(CellType.list[i])
-
-  fgseaRes2 <- data.frame(paste0(CellType.list[i]),fgseaRes)
-  colnames(fgseaRes2)[[1]] <- c("PhenoType")
-  GSEA_Large_Female.df <- rbind(GSEA_Large_Female.df,fgseaRes2 )
-
-  topPathways2 <- data.frame(paste0(CellType.list[i]),topPathways)
-  colnames(topPathways2)[[1]] <- c("PhenoType")
-  GSEA_Large_Female.df.TOP <- rbind(GSEA_Large_Female.df.TOP,topPathways2 )
-
-  rm(gseaDat,ranks,pathwaysH,fgseaRes,fgseaRes2,plotEnrichmen,Sum,topPathways,topPathways2)
-
-}
-
-dev.off()
-
-## GSEA_Large_Female.Sum.TOP ##
-GSEA_Large_Female.Sum.TOP <- rbind(GSEA_Large_Female.df.TOP)
-GSEA_Large_Female.Sum.TOP <- GSEA_Large_Female.Sum.TOP[,!colnames(GSEA_Large_Female.Sum.TOP) %in% c("leadingEdge")]
-write.table(GSEA_Large_Female.Sum.TOP, file=paste0(Subfolder.Path,"/PBMC_GSEA_Pathway_LargeTOP_SSA_Female.txt"),sep="\t",
+GSEA_Large_Female.df.TOP <- GSEA_SSA_Female.lt[["GSEA_Large.df.TOP"]]
+GSEA_Large_Female.df.TOP <- GSEA_Large_Female.df.TOP[,!colnames(GSEA_Large_Female.df.TOP) %in% c("leadingEdge")]
+write.table(GSEA_Large_Female.df.TOP, file=paste0(Subfolder.Path,"/PBMC_GSEA_Pathway_LargeTOP_SSA_Female.txt"),sep="\t",
             row.names=F, quote = FALSE)
 
 ##### Bubble plot #####
