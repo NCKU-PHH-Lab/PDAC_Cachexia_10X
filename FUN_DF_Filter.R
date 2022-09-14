0bf54c91ea953246f241109e788a4cd08b41fb2a
CharleneMSI <q2330416@gmail.com>
2022-09-14 17:49:38 +0800

    Parameter setting

--- FUN_DF_Filter.R
+++ FUN_DF_Filter.R
@@ -1,8 +1,9 @@
 DF_Filter <- function(df,Mode = "Keyword",  # Mode = ("KeyWordFilter", "LogicalJudgment")
-                      Condition)
+                      Condition = list(ColNa = "Colname", Value = 1, LogicalSet = ">"))
+                     #(for Keyword) Conditio  = list(ColNa = "Colname", Exact_Match = "No", KeyWord = "Mac")
 {
   if(Mode == "LogicalJudgment"){
-
+    # OUTPUT <- df[abs(df$NES) > 1,]
   }else{
 
   }
