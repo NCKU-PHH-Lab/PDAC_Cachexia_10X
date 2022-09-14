1d114744f1d85073a6368a330fb45455c813d4c4
CharleneMSI <q2330416@gmail.com>
2022-09-15 00:14:44 +0800

    Change the compare order

--- #_Seurat_Cachexia_PBMC_10_CellChat.R
+++ #_Seurat_Cachexia_PBMC_10_CellChat.R
@@ -97,8 +97,8 @@
 ##### Merge cellchat #####
 
   ##### Load rds #####
-  cellchat.EO <- readRDS("D:/Dropbox/##_GitHub/##_PHH_Lab/PDAC_Cachexia_10X/2022-09-09_PBMC_Main/PBMC_CellCell_Interaction/ECM_EO_CellChat.rds")
   cellchat.LO <- readRDS("D:/Dropbox/##_GitHub/##_PHH_Lab/PDAC_Cachexia_10X/2022-09-09_PBMC_Main/PBMC_CellCell_Interaction/ECM_LO_CellChat.rds")
+  cellchat.EO <- readRDS("D:/Dropbox/##_GitHub/##_PHH_Lab/PDAC_Cachexia_10X/2022-09-09_PBMC_Main/PBMC_CellCell_Interaction/ECM_EO_CellChat.rds")
 
 
   object.list <- list(EO = cellchat.EO, LO = cellchat.LO)
