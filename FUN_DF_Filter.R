5cf24df6f5254051454df5f936eefb1fc1aab869
CharleneMSI <q2330416@gmail.com>
2022-09-14 17:38:25 +0800

    Ori

---
+++ FUN_DF_Filter.R
@@ -0,0 +1,19 @@
+DF_Filter <- function(df,Mode = "Keyword",  # Mode = ("KeyWordFilter", "LogicalJudgment")
+                      Condition)
+{
+  if(Mode == "LogicalJudgment"){
+
+  }else{
+
+  }
+
+
+
+
+
+  ## Output setting ##
+  OUTPUT
+return(OUTPUT)
+
+
+}
