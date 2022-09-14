9f662224da2f83922ed73d19aa02c36b51522be9
CharleneMSI <q2330416@gmail.com>
2022-09-15 00:16:51 +0800

    Change the compare order

--- #_Seurat_Cachexia_SC_10_CellChat.R
+++ #_Seurat_Cachexia_SC_10_CellChat.R
@@ -101,7 +101,7 @@
   cellchat.EO <- readRDS("D:/Dropbox/##_GitHub/##_PHH_Lab/PDAC_Cachexia_10X/2022-09-09_SC_Main/SC_CellCell_Interaction/ECM_EO_CellChat.rds")
   cellchat.LO <- readRDS("D:/Dropbox/##_GitHub/##_PHH_Lab/PDAC_Cachexia_10X/2022-09-09_SC_Main/SC_CellCell_Interaction/ECM_LO_CellChat.rds")
 
-  object.list <- list(EO = cellchat.EO, LO = cellchat.LO)
+  object.list <- list(LO = cellchat.LO, EO = cellchat.EO)
   cellchat <- mergeCellChat(object.list, add.names = names(object.list))
 
   cellchat
