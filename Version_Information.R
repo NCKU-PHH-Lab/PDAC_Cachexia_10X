6c19f664b2de441829f855726f049faa4f8f2824
CharleneMSI <q2330416@gmail.com>
2022-09-29 20:40:22 +0800

    Version information

---
+++ Version_Information.R
@@ -0,0 +1,18 @@
+##### Version information ######
+# platform       x86_64-w64-mingw32
+# arch           x86_64
+# os             mingw32
+# system         x86_64, mingw32
+# status
+# major          4
+# minor          1.2
+# year           2021
+# month          11
+# day            01
+# svn rev        81115
+# language       R
+# version.string R version 4.1.2 (2021-11-01)
+# nickname       Bird Hippie
+#--------------------------------#
+# R version 4.1.2
+# Seurat 4.0.2
