043f0b9464af5a1c172a713440340913129094b5
CharleneMSI <q2330416@gmail.com>
2022-10-03 01:14:27 +0800

    Add Abbreviation Note

---
+++ Abbreviation_Note.R
@@ -0,0 +1,12 @@
+##### Abbreviation Note #####
+# CCM: Cancer Cachexia Marker
+# SPA: Sex Pooled Analysis
+# SSA: Sex Separated Analysis
+# I:Intersection ; F: Female ; M: Male
+
+# EO: Early Onset
+# LO: Late Onset
+# CT: Cell Type
+
+# list:lt
+# dataframe: df
