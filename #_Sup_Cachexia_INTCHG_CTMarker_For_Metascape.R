f8e286c02555b9b12f237ff1cc6d3f97fd1de283
CharleneMSI <q2330416@gmail.com>
2022-10-03 16:30:46 +0800

    Clean up the code

--- #_Sup_Cachexia_INTCHG_CTMarker_For_Metascape.R
+++ #_Sup_Cachexia_INTCHG_CTMarker_For_Metascape.R
@@ -1,43 +1,50 @@
-## INTCHG: Interchangeable
-  scRNA.SeuObj <- PBMC.combined
-  # scRNA.SeuObj <- SC.combined
-
 # ##### Presetting ######
 #   rm(list = ls()) # Clean variable
 #   memory.limit(150000)
 #
-# ##### Load libray #####
-#   library(tidyverse)
-#   library(stringr)
-#   library(ggplot2)
-#   library(Seurat)
+#
 #
 # # ##### Load RData  #####
 #   load("D:/Dropbox/##_GitHub/##_PHH_Lab/PDAC_Cachexia_10X/2022-09-09_PBMC_Main/08_2_Find_CCmarker_in_different_Cell_type_and_VolcanoPlot(SPA).RData")
 #
 #
-# ##### Current path and new folder setting #####
-#   Version = paste0(Sys.Date(),"_","PBMC_CTMarker")
-#   Save.Path = paste0(getwd(),"/",Version)
-#   dir.create(Save.Path)
-#   SampleType="PBMC"
-#
-#   PathName <- Save.Path
+
+##### Load libray #####
+  library(tidyverse)
+  library(stringr)
+  library(ggplot2)
+  library(Seurat)
+
+## INTCHG: Interchangeable
+  # For PBMC
+  scRNA.SeuObj <- PBMC.combined
+  SampleType = "PBMC"
+
+  ## For SC
+  # scRNA.SeuObj <- SC.combined
+  # SampleType = "SC"
+
+##### Current path and new folder setting #####
+  Version = paste0(Sys.Date(),"_",SampleType,"_CTMarker")
+  Save.Path = paste0(getwd(),"/",Version)
+  dir.create(Save.Path)
+
+  PathName <- Save.Path
 
 ##### Cluster marker gene for Metascape #####
   Idents(scRNA.SeuObj) <- "celltype"
   set.seed(1) # Fix the seed
-  PBMC_markers_CT <- FindAllMarkers(scRNA.SeuObj, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
+  Markers_CT <- FindAllMarkers(scRNA.SeuObj, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
 
 
   # Filter the top markers and plot the heatmap
   top_NSet = 25
-  PBMC_markers_CT %>%
+  Markers_CT %>%
     group_by(cluster) %>%
     top_n(n = top_NSet, wt = avg_log2FC) -> top_N
   scRNA.SeuObj <- ScaleData(scRNA.SeuObj, verbose = FALSE)
   DoHeatmap(scRNA.SeuObj, features = top_N$gene) + NoLegend()
-  write.table(top_N, file=paste0(PathName,"/PBMC_CT_top",top_NSet,"Gene.txt"),sep="\t", row.names=T
+  write.table(top_N, file=paste0(PathName,"/",SampleType,"_CT_top",top_NSet,"Gene.txt"),sep="\t", row.names=T
               , quote = FALSE)
 
   Cluster_Marker.lst <- list()
@@ -61,6 +68,6 @@
   colnames(Cluster_Marker.df) <- Cluster.list
   #Cluster_Marker.df <- as.data.frame(t(Cluster_Marker.df))
   write.table(Cluster_Marker.df,
-              file=paste0(PathName,"/PBMC_CT_top",
+              file=paste0(PathName,"/",SampleType,"_CT_top",
                           top_NSet,"Gene_forMetascape.csv"),sep=",",
               row.names=F,col.names = T, quote = FALSE)
