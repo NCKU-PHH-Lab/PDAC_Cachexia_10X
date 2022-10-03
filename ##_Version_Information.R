6a226f9af4b4012553d11177a450798a7994e1aa
CharleneMSI <q2330416@gmail.com>
2022-10-03 15:01:34 +0800

    Version information

---
+++ ##_Version_Information.R
@@ -0,0 +1,63 @@
+##### Version information #####
+#### R Version ####
+# > version
+
+# platform       x86_64-w64-mingw32
+# arch           x86_64
+# os             mingw32
+# system         x86_64, mingw32
+# status
+# major          4
+# minor          1.2
+# year           2021
+# month          11
+# day            01
+# svn rev        81115
+# language       R
+# version.string R version 4.1.2 (2021-11-01)
+# nickname       Bird Hippie
+
+
+
+#### Packages information ####
+# > sessionInfo()
+
+# R version 4.1.2 (2021-11-01)
+# Platform: x86_64-w64-mingw32/x64 (64-bit)
+# Running under: Windows 10 x64 (build 19044)
+#
+# Matrix products: default
+#
+# locale:
+#   [1] LC_COLLATE=Chinese (Traditional)_Taiwan.950  LC_CTYPE=Chinese (Traditional)_Taiwan.950    LC_MONETARY=Chinese (Traditional)_Taiwan.950
+# [4] LC_NUMERIC=C                                 LC_TIME=Chinese (Traditional)_Taiwan.950
+#
+# attached base packages:
+#   [1] stats     graphics  grDevices utils     datasets  methods   base
+#
+# other attached packages:
+#   [1] dplyr_1.0.9           magrittr_2.0.1        stringr_1.4.0         aplot_0.1.6           broom_1.0.0           ggpmisc_0.5.0
+# [7] ggpp_0.4.4            ggplot2_3.3.6         patchwork_1.1.1       ifnb.SeuratData_3.1.0 SeuratData_0.2.1      sp_1.5-0
+# [13] SeuratObject_4.1.0    Seurat_4.1.1
+#
+# loaded via a namespace (and not attached):
+#   [1] Rtsne_0.16            colorspace_2.0-3      deldir_1.0-6          ellipsis_0.3.2        ggridges_0.5.3        rstudioapi_0.14
+# [7] spatstat.data_2.2-0   leiden_0.4.2          listenv_0.8.0         MatrixModels_0.5-0    ggrepel_0.9.1         fansi_1.0.3
+# [13] codetools_0.2-18      splines_4.1.2         polyclip_1.10-0       jsonlite_1.8.0        ica_1.0-3             cluster_2.1.2
+# [19] png_0.1-7             rgeos_0.5-9           uwot_0.1.11           shiny_1.7.2           sctransform_0.3.3     spatstat.sparse_2.1-1
+# [25] BiocManager_1.30.18   compiler_4.1.2        httr_1.4.4            backports_1.4.1       assertthat_0.2.1      Matrix_1.3-4
+# [31] fastmap_1.1.0         lazyeval_0.2.2        cli_3.3.0             later_1.3.0           quantreg_5.94         htmltools_0.5.3
+# [37] tools_4.1.2           igraph_1.3.4          gtable_0.3.0          glue_1.6.2            RANN_2.6.1            reshape2_1.4.4
+# [43] rappdirs_0.3.3        tinytex_0.41          Rcpp_1.0.9            scattermore_0.8       vctrs_0.4.1           nlme_3.1-153
+# [49] progressr_0.10.1      lmtest_0.9-40         spatstat.random_2.2-0 xfun_0.31             globals_0.16.0        mime_0.12
+# [55] miniUI_0.1.1.1        lifecycle_1.0.1       irlba_2.3.5           goftest_1.2-3         future_1.27.0         MASS_7.3-54
+# [61] zoo_1.8-10            scales_1.2.1          spatstat.core_2.4-4   promises_1.2.0.1      spatstat.utils_2.3-1  parallel_4.1.2
+# [67] SparseM_1.81          RColorBrewer_1.1-3    yaml_2.2.2            reticulate_1.25       pbapply_1.5-0         gridExtra_2.3
+# [73] ggfun_0.0.6           yulab.utils_0.0.5     rpart_4.1-15          stringi_1.7.8         rlang_1.0.4           pkgconfig_2.0.3
+# [79] matrixStats_0.62.0    lattice_0.20-45       ROCR_1.0-11           purrr_0.3.4           tensor_1.5            htmlwidgets_1.5.4
+# [85] cowplot_1.1.1         tidyselect_1.1.2      parallelly_1.32.1     RcppAnnoy_0.0.19      plyr_1.8.7            R6_2.5.1
+# [91] generics_0.1.3        DBI_1.1.3             pillar_1.8.1          withr_2.5.0           mgcv_1.8-38           fitdistrplus_1.1-8
+# [97] survival_3.3-1        abind_1.4-5           tibble_3.1.8          future.apply_1.9.0    crayon_1.5.1          KernSmooth_2.23-20
+# [103] utf8_1.2.2            spatstat.geom_2.4-0   plotly_4.10.0         grid_4.1.2            data.table_1.14.2     digest_0.6.29
+# [109] xtable_1.8-4          tidyr_1.2.0           httpuv_1.6.5          gridGraphics_0.5-1    munsell_0.5.0         ggplotify_0.1.0
+# [115] viridisLite_0.4.1
