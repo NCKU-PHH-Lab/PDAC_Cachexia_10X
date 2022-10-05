56f13775b392687063f99b39aa0193b2f76ac97e
CharleneMSI <q2330416@gmail.com>
2022-10-05 16:47:53 +0800

    Add Sub cell type marks in UMAP

--- #_01-06_Cachexia_SC_Main.R
+++ #_01-06_Cachexia_SC_Main.R
@@ -632,6 +632,34 @@
   FeaturePlot(SC.combined, features = c("Il4ra"), min.cutoff = "q9",ncol = 3)
   FeaturePlot(SC.combined, features = c("Il13ra1"), min.cutoff = "q9",ncol = 3)
   FeaturePlot(SC.combined, features = c("Stat6"), min.cutoff = "q9",ncol = 3)
+  FeaturePlot(SC.combined, features = c("Il1b"), min.cutoff = "q9",ncol = 3)
+
+
+  pdf(
+    file = paste0(Save.Path,"/SC_UMAP_SubCTMarkers",".pdf"),
+    width = 10,  height = 8
+  )
+
+    # https://pubmed.ncbi.nlm.nih.gov/31197017/
+    # iCAF
+    FeaturePlot(SC.combined, features = c("Clec3b","Col14a1","Has1","Il6"), min.cutoff = "q9",ncol = 2)
+    # apCAF
+    FeaturePlot(SC.combined, features = c("H2-Ab1","Cd74","Saa3","Slpi"), min.cutoff = "q9",ncol = 2)
+    # myCAF
+    FeaturePlot(SC.combined, features = c("Tagln","Thy1","Col12a1","Thbs2"), min.cutoff = "q9",ncol = 2)
+
+    # EMT-like cells
+    FeaturePlot(SC.combined, features = c("Cdkn2a","S100a6","Igfbp4","Sparc","Vim","Spp1"), min.cutoff = "q9",ncol = 2)
+  dev.off()
+
+  pdf(
+    file = paste0(Save.Path,"/SC_UMAP_SubCTMarkers2",".pdf"),
+    width = 10,  height = 14
+  )
+
+    # EMT-like cells
+    FeaturePlot(SC.combined, features = c("Cdkn2a","S100a6","Igfbp4","Sparc","Vim","Spp1"), min.cutoff = "q9",ncol = 2)
+  dev.off()
 
   rm(Test,Test2)
 # -------------------------------------- #
