6b6cc085e23bcd32bfd978336bee0ba13b596b4a
Charlene <q2330416@gmail.com>
2022-10-12 18:07:44 +0800

    RunAll

---
+++ RunAll.R
@@ -0,0 +1,15 @@
+## For PBMC
+source("#_01-06_Cachexia_PBMC_Main.R")
+source("#_07_Cachexia_INTCHG_Count_Cell_Number.R")
+source("#_08_1-2_Cachexia_INTCHG_Find_Markers.R")
+source("#_08_3_Cachexia_INTCHG_Find_Markers_GeneCount_CTCount.R")
+source("#_09_Cachexia_INTCHG_Enrichment_Analysis.R")
+source("#_10_Cachexia_INTCHG_CellChat.R")
+
+## For SC
+source("#_01-06_Cachexia_SC_Main.R")
+source("#_07_Cachexia_INTCHG_Count_Cell_Number.R")
+source("#_08_1-2_Cachexia_INTCHG_Find_Markers.R")
+source("#_08_3_Cachexia_INTCHG_Find_Markers_GeneCount_CTCount.R")
+source("#_09_Cachexia_INTCHG_Enrichment_Analysis.R")
+source("#_10_Cachexia_INTCHG_CellChat.R")
