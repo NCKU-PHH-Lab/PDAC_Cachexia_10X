9bc9f69c3f316000cec600dd3593fc7abd295d6d
CharleneMSI <q2330416@gmail.com>
2022-10-14 16:50:06 +0800

    More Test

--- RunAll.R
+++ RunAll.R
@@ -5,6 +5,16 @@ source("#_08_1-2_Cachexia_INTCHG_Find_Markers.R")
 source("#_08_3_Cachexia_INTCHG_Find_Markers_GeneCount_CTCount.R")
 source("#_09_Cachexia_INTCHG_Enrichment_Analysis.R")
 source("#_10_Cachexia_INTCHG_CellChat.R")
+source("#_Sup_Cachexia_INTCHG_Barplot_PValue.R")
+source("#_Sup_Cachexia_INTCHG_Beautify_the_Figures.R")
+source("#_Sup_Cachexia_INTCHG_CellChat_Barplot_PValue.R")
+source("#_Sup_Cachexia_INTCHG_CellChat_Heatmap_PValue.R")
+source("#_Sup_Cachexia_INTCHG_CellChat_ModifyPlot.R")
+source("#_Sup_Cachexia_INTCHG_CellChat_ViolinBar_PValue.R")
+source("#_Sup_Cachexia_INTCHG_CTMarker_For_Metascape.R")
+source("#_Sup_Cachexia_INTCHG_Enrichment_Analysis_SpecificCondition.R")
+source("#_Sup_Cachexia_INTCHG_EnrichmentUMAP.R")
+
 
 ## For SC
 source("#_01-06_Cachexia_SC_Main.R")
@@ -13,3 +23,18 @@ source("#_08_1-2_Cachexia_INTCHG_Find_Markers.R")
 source("#_08_3_Cachexia_INTCHG_Find_Markers_GeneCount_CTCount.R")
 source("#_09_Cachexia_INTCHG_Enrichment_Analysis.R")
 source("#_10_Cachexia_INTCHG_CellChat.R")
+source("#_Sup_Cachexia_INTCHG_Barplot_PValue.R")
+source("#_Sup_Cachexia_INTCHG_Beautify_the_Figures.R")
+source("#_Sup_Cachexia_INTCHG_CellChat_Barplot_PValue.R")
+source("#_Sup_Cachexia_INTCHG_CellChat_Heatmap_PValue.R")
+source("#_Sup_Cachexia_INTCHG_CellChat_ModifyPlot.R")
+source("#_Sup_Cachexia_INTCHG_CellChat_ViolinBar_PValue.R")
+source("#_Sup_Cachexia_INTCHG_CTMarker_For_Metascape.R")
+source("#_Sup_Cachexia_INTCHG_Enrichment_Analysis_SpecificCondition.R")
+source("#_Sup_Cachexia_INTCHG_EnrichmentUMAP.R")
+
+
+## Other Test
+source("#_Sup_Cachexia_INTCHG_CCMarker_Heatmap.R")
+source("#_Sup_Cachexia_PBMC_DR_Without_Batch_Correction.R")
+
