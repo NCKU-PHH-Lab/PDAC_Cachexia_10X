ebce323c052c2bac7a4d091b0bb1be6beb765bb5
Charlene <q2330416@gmail.com>
2022-10-18 00:39:30 +0800

    Change the order

--- RunAll.R
+++ RunAll.R
@@ -5,15 +5,17 @@ source("#_08_1-2_Cachexia_INTCHG_Find_Markers.R")
 source("#_08_3_Cachexia_INTCHG_Find_Markers_GeneCount_CTCount.R")
 source("#_09_Cachexia_INTCHG_Enrichment_Analysis.R")
 source("#_10_Cachexia_INTCHG_CellChat.R")
-source("#_Sup_Cachexia_INTCHG_Barplot_PValue.R")
+
 source("#_Sup_Cachexia_INTCHG_Beautify_the_Figures.R")
+source("#_Sup_Cachexia_INTCHG_CTMarker_For_Metascape.R")
+source("#_Sup_Cachexia_INTCHG_EnrichmentUMAP.R")
+source("#_Sup_Cachexia_INTCHG_Enrichment_Analysis_SpecificCondition.R")
 source("#_Sup_Cachexia_INTCHG_CellChat_Barplot_PValue.R")
 source("#_Sup_Cachexia_INTCHG_CellChat_Heatmap_PValue.R")
 source("#_Sup_Cachexia_INTCHG_CellChat_ModifyPlot.R")
 source("#_Sup_Cachexia_INTCHG_CellChat_ViolinBar_PValue.R")
-source("#_Sup_Cachexia_INTCHG_CTMarker_For_Metascape.R")
-source("#_Sup_Cachexia_INTCHG_Enrichment_Analysis_SpecificCondition.R")
-source("#_Sup_Cachexia_INTCHG_EnrichmentUMAP.R")
+source("#_Sup_Cachexia_INTCHG_Barplot_PValue.R")
+
 
 rm(list = ls()) # Clean variable
 memory.limit(150000)
@@ -25,16 +27,16 @@ source("#_08_1-2_Cachexia_INTCHG_Find_Markers.R")
 source("#_08_3_Cachexia_INTCHG_Find_Markers_GeneCount_CTCount.R")
 source("#_09_Cachexia_INTCHG_Enrichment_Analysis.R")
 source("#_10_Cachexia_INTCHG_CellChat.R")
-source("#_Sup_Cachexia_INTCHG_Barplot_PValue.R")
+
 source("#_Sup_Cachexia_INTCHG_Beautify_the_Figures.R")
+source("#_Sup_Cachexia_INTCHG_CTMarker_For_Metascape.R")
+source("#_Sup_Cachexia_INTCHG_EnrichmentUMAP.R")
+source("#_Sup_Cachexia_INTCHG_Enrichment_Analysis_SpecificCondition.R")
 source("#_Sup_Cachexia_INTCHG_CellChat_Barplot_PValue.R")
 source("#_Sup_Cachexia_INTCHG_CellChat_Heatmap_PValue.R")
 source("#_Sup_Cachexia_INTCHG_CellChat_ModifyPlot.R")
 source("#_Sup_Cachexia_INTCHG_CellChat_ViolinBar_PValue.R")
-source("#_Sup_Cachexia_INTCHG_CTMarker_For_Metascape.R")
-source("#_Sup_Cachexia_INTCHG_Enrichment_Analysis_SpecificCondition.R")
-source("#_Sup_Cachexia_INTCHG_EnrichmentUMAP.R")
-
+source("#_Sup_Cachexia_INTCHG_Barplot_PValue.R")
 
 ## Other Test
 source("#_Sup_Cachexia_INTCHG_CCMarker_Heatmap.R")
