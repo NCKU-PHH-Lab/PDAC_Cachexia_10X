23091319fe7a4715bb7251901f476e3ef9481446
CharleneMSI <q2330416@gmail.com>
2023-11-22 09:17:33 -0600

    More plot

--- 20231121_MarkerScores.R
+++ 20231121_MarkerScores.R
@@ -10,8 +10,12 @@ if(!require("AUCell")) install.packages("AUCell"); library(AUCell)
 
 #### Load data ####
 ## load seuratObject
-load("D:/Dropbox/##_GitHub/##_PHH_Lab/PDAC_Cachexia_10X/2022-12_Results_FollowUpAnalysis/2022-10-17_PBMC_Main/06_Cell_type_annotation.RData")
-seuratObject <- PBMC.combined
+# load("D:/Dropbox/##_GitHub/##_PHH_Lab/PDAC_Cachexia_10X/2022-12_Results_FollowUpAnalysis/2022-10-17_PBMC_Main/06_Cell_type_annotation.RData")
+# seuratObject <- PBMC.combined
+
+load("D:/Dropbox/##_GitHub/##_PHH_Lab/PDAC_Cachexia_10X/2022-12_Results_FollowUpAnalysis/2022-10-17_SC_Main/06_Cell_type_annotation.RData")
+seuratObject <- SC.combined
+
 rm(list = setdiff(ls(), "seuratObject"))
 
 colnames(seuratObject@meta.data)[colnames(seuratObject@meta.data) == "celltype"] <- "Cell_Type"
@@ -21,7 +25,7 @@ source("FUN_Beautify_ggplot.R")
 source("FUN_Beautify_UMAP.R")
 p1 <- DimPlot(seuratObject, reduction = "umap", group.by = "sample") # %>% BeautifyggPlot(.,LegPos = c(0.85, 0.15),AxisTitleSize=1.1)
 p2 <- DimPlot(seuratObject, reduction = "umap", group.by = "Cell_Type", label = TRUE) # %>% BeautifyggPlot(.,LegPos = c(0.85, 0.15),AxisTitleSize=1.1)
-p3 <- DimPlot(seuratObject, reduction = "umap", group.by = "seurat_clusters") # %>% BeautifyggPlot(.,LegPos = c(0.85, 0.15),AxisTitleSize=1.1)
+p3 <- DimPlot(seuratObject, reduction = "umap", group.by = "seurat_clusters", label = TRUE) # %>% BeautifyggPlot(.,LegPos = c(0.85, 0.15),AxisTitleSize=1.1)
 p4 <- DimPlot(seuratObject, reduction = "umap", group.by = "Cachexia") # %>% BeautifyggPlot(.,LegPos = c(0.85, 0.15),AxisTitleSize=1.1)
 p1 + p2 + p3 + p4
 
@@ -30,7 +34,7 @@ file_path <- "D:/Dropbox/##_GitHub/##_PHH_Lab/PDAC_Cachexia_10X/Input_Markers/PM
 data <- read.table(file_path, header=TRUE, sep="\t", skip = 1)  # 跳过第一行
 
 file_path <- "D:/Dropbox/##_GitHub/##_PHH_Lab/PDAC_Cachexia_10X/Input_Markers/PMID37914939/41586_2023_6685_MOESM4_ESM_scRNAseq_KPC_TAMs_Markers.txt"
-data <- read.table(file_path, header=TRUE, sep="\t")  # 跳过第一行
+data <- read.table(file_path, header=TRUE, sep="\t")
 data <- data %>%
   dplyr::rename(Subset = Cell_population) %>%
   dplyr::rename(Gene_Mouse = Gene)
@@ -56,16 +60,12 @@ standard_list <- setNames(subset_genes$Genes, subset_genes$Subset)
 #### AUCell ####
 library(AUCell)
 
-# 提取基因表达矩阵
 exprMatrix <- GetAssayData(seuratObject, assay = "RNA", slot = "data")
-
-# 创建基因排名
 rankings <- AUCell::AUCell_buildRankings(exprMatrix)
 
 expr_genes <- rownames(exprMatrix)
-# 初始化用于保存 AUCell 分数的列表
-subset_scores <- list()
 
+subset_scores <- list() # 初始化用于保存 AUCell 分数的列表
 # 计算每个子集的 AUCell 分数
 for(subset in names(standard_list)) {
   # 确保基因名称一致性
@@ -82,28 +82,48 @@ for(subset in names(standard_list)) {
   }
 }
 
-# 检查计算结果
-print(subset_scores)
 
-# 将 AUCell 分数保存到文件中
-write.table(subset_scores, file = "subset_scores.txt", sep = "\t", quote = FALSE)
-
+print(subset_scores) # 检查计算结果
 # 逐个检查每个子集的 AUCell 分数
 for(subset in names(subset_scores)) {
   cat("Subset:", subset, "\n")
   print(head(subset_scores[[subset]]))  # 只打印每个子集的前几个分数
 }
 
+## Export txt
+write.table(subset_scores, file = "subset_scores.txt", sep = "\t", quote = FALSE)
 
 
+
+# # 将 AUCell 分数添加到 Seurat 对象的元数据中
+# for(subset in names(subset_scores)) {
+#   # 假设 subset_scores[[subset]] 是一个向量，包含每个细胞的 AUCell 分数
+#   # seuratObject <- AddMetaData(seuratObject, metadata = subset_scores[[subset]], col.name = paste0(subset, "_AUCell"))
+#   seuratObject <- AddMetaData(seuratObject, metadata = as.numeric(subset_scores[[subset]]@assays@data@listData[["AUC"]]), col.name = paste0(subset, "_AUCell"))
+#
+# }
 
 # 将 AUCell 分数添加到 Seurat 对象的元数据中
 for(subset in names(subset_scores)) {
-  # 假设 subset_scores[[subset]] 是一个向量，包含每个细胞的 AUCell 分数
-  # seuratObject <- AddMetaData(seuratObject, metadata = subset_scores[[subset]], col.name = paste0(subset, "_AUCell"))
-  seuratObject <- AddMetaData(seuratObject, metadata = as.numeric(subset_scores[[subset]]@assays@data@listData[["AUC"]]), col.name = paste0(subset, "_AUCell"))
+  # 提取 AUCell 分数和细胞 ID
+  auc_data <- subset_scores[[subset]]@assays@data@listData[["AUC"]]
+  cell_ids <- colnames(auc_data)
 
+  # 确保 Seurat 对象中的细胞 ID 与 AUCell 分数的细胞 ID 匹配
+  if (all(colnames(seuratObject) %in% cell_ids)) {
+    # 提取与 Seurat 对象细胞顺序相匹配的 AUCell 分数
+    matched_auc_scores <- auc_data[, match(colnames(seuratObject), cell_ids)]
+
+    # 添加匹配的 AUCell 分数到 Seurat 对象的元数据中
+    metadata_col_name <- paste0(subset, "_AUCell")
+    seuratObject[[metadata_col_name]] <- matched_auc_scores
+  } else {
+    warning(paste("Not all cells in Seurat object have matching AUCell scores in subset", subset))
+  }
 }
+
+# 检查添加结果
+head(seuratObject@meta.data)
 
 colnames(seuratObject@meta.data) <- gsub("\\.\\.", "+ ", colnames(seuratObject@meta.data))
 colnames(seuratObject@meta.data) <- gsub("\\.", " ", colnames(seuratObject@meta.data))
@@ -127,3 +147,30 @@ combined_plot
 
 combined_plot2 <- wrap_plots(list(p1,p2,p3,p4), ncol = 2)  # 根据需要调整列数
 combined_plot2
+
+
+#############################################
+
+
+# 定义要绘制的聚类
+clusters_to_plot <- c("4", "5", "8", "10", "11")
+
+# 对于每个子集，绘制特定聚类的 AUCell 分数分布图
+plots_2 <- list()
+for(subset in names(subset_scores)) {
+  feature_to_plot <- paste0(subset, "_AUCell")
+  # 创建符合条件的子 Seurat 对象
+  subset_seuratObject <- subset(seuratObject, subset = seurat_clusters %in% clusters_to_plot)
+  p <- VlnPlot(subset_seuratObject, features = feature_to_plot, group.by = "seurat_clusters")
+  plots_2[[subset]] <- p
+}
+
+# 使用 patchwork 组合多个图形
+if(!require("patchwork")) {
+  install.packages("patchwork")
+  library(patchwork)
+}
+combined_plot_2 <- wrap_plots(plots_2, ncol = 2)  # 根据需要调整列数
+
+# 显示组合图
+combined_plot_2
