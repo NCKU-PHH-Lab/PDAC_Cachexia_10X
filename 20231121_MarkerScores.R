786171b78d1ca4c3a8ebba8802e30b618ec8f06c
CharleneMSI <q2330416@gmail.com>
2023-12-01 13:38:40 -0600

    Add PMID36781852_Mph

--- 20231121_MarkerScores.R
+++ 20231121_MarkerScores.R
@@ -12,8 +12,8 @@ if(!require("Hmisc")) install.packages("Hmisc"); library(Hmisc)
 
 ##### Set Para #####
 ## Set Para
-Set_Dataset <- "PBMC" # "PBMC" # "SC"
-Set_MarkerFile <- "PMID36352227_Mph" # "PMID36352227_Mph" # "KPCTAMs" # "HmMuConTAMs"
+Set_Dataset <- "SC" # "PBMC" # "SC"
+Set_MarkerFile <- "PMID36781852_Mph" # "PMID36781852_Mph"  # "PMID36352227_Mph" # "KPCTAMs" # "HmMuConTAMs"
 Set_ConvertGeneName = TRUE
 
 if(Set_Dataset == "SC"){
@@ -77,6 +77,12 @@ if(Set_MarkerFile == "HmMuConTAMs"){
     dplyr::rename(Subset = Cell.cluster) %>%
     dplyr::rename(Gene_Mouse = Gene)
   data <- data %>% dplyr::filter(grepl("^Mph_", Subset))
+}else if(Set_MarkerFile == "PMID36781852_Mph"){
+  file_path_Marker <- "D:/Dropbox/##_GitHub/##_PHH_Lab/PDAC_Cachexia_10X/Input_Markers/PMID36781852/PMID36781852_Mph.txt"
+  data <- read.table(file_path_Marker, header=TRUE, sep="\t")
+  data <- data %>%
+    dplyr::rename(Subset = CellType) %>%
+    dplyr::rename(Gene_Mouse = Gene)
 }
 
 if(Set_ConvertGeneName){
